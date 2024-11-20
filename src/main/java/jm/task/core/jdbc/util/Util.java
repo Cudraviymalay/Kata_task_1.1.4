6c7cb470afa1f4434e1e595be404ddb946f60bfb
Mahmutov Azat <cudraviymalay@gmail.com>
2024-11-20 20:58:17 +0300

    добавил логику в Util

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,9 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,5 +18,28 @@ public class Util {
 
         Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         return connection;
+    }
+
+    private static SessionFactory sessionFactory;
+
+    public static SessionFactory getSessionFactory() {
+
+        try {
+            Configuration configuration = new Configuration();
+
+            configuration.setProperty("hibernate.connection.url", URL);
+            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+            configuration.setProperty("hibernate.connection.username", USERNAME);
+            configuration.setProperty("hibernate.connection.password", PASSWORD);
+            configuration.setProperty("hibernate.current_session_context_class", "thread");
+            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            configuration.setProperty("hibernate.show_sql", "true");
+            configuration.addAnnotatedClass(User.class);
+
+            sessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+        return sessionFactory;
     }
 }
