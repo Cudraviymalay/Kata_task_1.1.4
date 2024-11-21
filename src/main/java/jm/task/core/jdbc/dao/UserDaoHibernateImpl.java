d46a216c60912f4c993e46ec199c9db37b857ad5
Mahmutov Azat <cudraviymalay@gmail.com>
2024-11-21 17:17:49 +0300

    сделал всё, но тест бракует save метод

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -9,34 +13,98 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
 
+        try (SessionFactory sessionFactory = Util.getSessionFactory()
+             ; Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+            session.createNativeQuery("create table if not exists Users (id int auto_increment primary key" +
+                    ", name varchar(45) not null, lastName varchar(45) not null, age int)").executeUpdate();
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void dropUsersTable() {
 
+        try (SessionFactory sessionFactory = Util.getSessionFactory()
+             ; Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+            session.createNativeQuery("drop table if exists Users").executeUpdate();
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
 
+        try (SessionFactory sessionFactory = Util.getSessionFactory()
+             ; Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.save(user);
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
 
+        try (SessionFactory sessionFactory = Util.getSessionFactory()
+             ; Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+
+        List<User> allUsers = new ArrayList<>();
+
+        try (SessionFactory sessionFactory = Util.getSessionFactory()
+             ; Session session = sessionFactory.getCurrentSession()) {
+
+            session.beginTransaction();
+            allUsers = session.createNativeQuery("select * from Users").getResultList();
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return allUsers;
     }
 
     @Override
     public void cleanUsersTable() {
+        try (SessionFactory sessionFactory = Util.getSessionFactory()
+             ; Session session = sessionFactory.getCurrentSession()) {
 
+            session.beginTransaction();
+            session.createNativeQuery("truncate table users").executeUpdate();
+            session.getTransaction().commit();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,16 +1,15 @@
 package jm.task.core.jdbc.util;
-
 import jm.task.core.jdbc.model.User;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
+import org.hibernate.cfg.Environment;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Util {
 
-    private static String URL = "jdbc:mysql://localhost:3306/kata_task_1.1.3";
+    private static String URL = "jdbc:mysql://localhost:3306/kata_task_1.1.4";
     private static String USERNAME = "root";
     private static String PASSWORD = "root505";
 
@@ -24,18 +23,18 @@ public class Util {
 
     public static SessionFactory getSessionFactory() {
 
-            Configuration configuration = new Configuration();
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
 
-            configuration.setProperty("hibernate.connection.url", URL);
-            configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
-            configuration.setProperty("hibernate.connection.username", USERNAME);
-            configuration.setProperty("hibernate.connection.password", PASSWORD);
-            configuration.setProperty("hibernate.current_session_context_class", "thread");
-            configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
-            configuration.setProperty("hibernate.show_sql", "true");
-            configuration.addAnnotatedClass(User.class);
+        configuration.setProperty("hibernate.connection.url", URL);
+        configuration.setProperty("hibernate.connection.driver_class", "com.mysql.cj.jdbc.Driver");
+        configuration.setProperty("hibernate.connection.username", USERNAME);
+        configuration.setProperty("hibernate.connection.password", PASSWORD);
+        configuration.setProperty("hibernate.current_session_context_class", "thread");
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+        configuration.setProperty("hibernate.show_sql", "true");
+        configuration.setProperty(Environment.HBM2DDL_AUTO, "");
 
-            sessionFactory = configuration.buildSessionFactory();
+        sessionFactory = configuration.buildSessionFactory();
 
         return sessionFactory;
     }
